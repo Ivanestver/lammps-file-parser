346f555402619cfd236714630cd7bfd4bbb5a39c
Ivan Patrenkov <ivanestver24@gmail.com>
2026-02-12 20:49:43 +0300

    add: pair and tuple structs

---
+++ structs/pair.go
@@ -0,0 +1,6 @@
+package structs
+
+type Pair[I1, I2 any] struct {
+	Item1 I1
+	Item2 I2
+}
---
+++ structs/tuple.go
@@ -0,0 +1,7 @@
+package structs
+
+type Tuple[I1, I2, I3 any] struct {
+	Item1 I1
+	Item2 I2
+	Item3 I3
+}
