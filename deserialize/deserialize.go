03a25b2a356ecc2ef27b0c3aaa3817cfaf9dba9e
Ivan Patrenkov <ivanestver24@gmail.com>
2026-05-10 21:43:34 +0300

    fix: remove an odd symbol

--- deserialize/deserialize.go
+++ deserialize/deserialize.go
@@ -2,6 +2,18 @@ package deserialize
 
 import "github.com/Ivanestver/lammps-file-parser/structs"
 
+/*
+Deserialize function converts a valid LAMMPS file into a set of objects
+that represents the file.
+
+Params:
+  - content: a LAMMPS file content
+  - filename: a LAMMPS file name
+
+Returns:
+  - LammpsStruct: the file contents' representation
+  - error: any error occured
+*/
 func Deserialize(content, fileName string) (*structs.LammpsStruct, error) {
 	loader := structs.LammpsLoader{}
 	if result, err := loader.Load(content); err != nil {
